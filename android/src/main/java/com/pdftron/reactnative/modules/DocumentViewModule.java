1671aa6d63e879a23ba179e02a8aaba872988750
vinzenz <vinzenz@nsynk.de>
2020-09-28 20:38:45 +0200

    recovered DocumentViewModule.java

--- android/src/main/java/com/pdftron/reactnative/modules/DocumentViewModule.java
+++ android/src/main/java/com/pdftron/reactnative/modules/DocumentViewModule.java
@@ -275,4 +275,274 @@ public class DocumentViewModule extends ReactContextBaseJavaModule implements Ac
     public void onNewIntent(Intent intent) {
 
     }
+
+
+
+    // CAT EUROPE
+
+    @ReactMethod
+    public void currentPage(final int tag, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int count = mDocumentViewInstance.currentPage(tag);
+                    promise.resolve(count);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+            }
+        });
+    }
+
+    
+
+    @ReactMethod
+    public void getDimensions(final int tag, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ReadableMap dimensions = mDocumentViewInstance.getDimensions(tag);
+                    promise.resolve(dimensions);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void jumpTo(final int tag, int page_num, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    mDocumentViewInstance.jumpTo(tag, page_num);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void rotate(final int tag, boolean ccw, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    mDocumentViewInstance.rotate(tag, ccw);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void toggleSlider(final int tag, boolean toggle, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    mDocumentViewInstance.toggleSlider(tag, toggle);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+
+
+    @ReactMethod
+    public void search(final int tag, String searchString, boolean isCase, boolean isWhole, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ReadableArray results = mDocumentViewInstance.search(tag, searchString, isCase, isWhole);
+                    promise.resolve(results);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+
+    @ReactMethod
+    public void getOutline(final int tag, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ReadableArray outline = mDocumentViewInstance.getOutline(tag);
+                    promise.resolve(outline);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+
+    @ReactMethod
+    public void getThumbnail(final int tag, int page, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.getThumbnail(tag, page, promise);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void abortGetThumbnail(final int tag, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.abortGetThumbnail(tag);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void findText(final int tag, String searchString, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.findText(tag, searchString);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+    @ReactMethod
+    public void cancelFindText(final int tag, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.cancelFindText(tag);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void findTextResult(final int tag, boolean nextprev, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.findTextResult(tag, nextprev);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void appendSchoolLogo(final int tag, String base64str, boolean isDuplex, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.appendSchoolLogo(tag, base64str, isDuplex);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void changeBackground(final int tag, int r, int g, int b, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.changeBackground(tag, r, g, b);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+    @ReactMethod
+    public void setContinuous(final int tag, boolean toggle, final Promise promise) {
+        getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mDocumentViewInstance.setContinuous(tag, toggle);
+                    promise.resolve(null);
+                } catch (Exception ex) {
+                    promise.reject(ex);
+                }
+
+            }
+        });
+    }
+
+
+
+
+
+
+
+
 }
